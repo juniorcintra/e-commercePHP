6e3c01579b23acc32c9356b15a43285c0aa1ab05
juniorcintra <junior.developer@outlook.com.br>
2019-03-13 12:07:58 -0300

    Ajustando lists produto

--- site.php
+++ site.php
@@ -21,15 +21,29 @@ $app->get('/', function() {
 
 $app->get("/category/:idcategory", function($idcategory) {
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1; 
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i=1; $i <= $pagination['pages']; $i++) { 
+		array_push($pages, [
+			'link'=>'/category/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
 
 	$page = new Page();
 
 	$page->setTpl("category", array(
 		'category'=>$category->getValues(),
-		'products'=>Product::checkList($category->getProduct())
+		'products'=>$pagination["data"],
+		'pages'=>$pages
 	));
 });
 

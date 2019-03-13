54b5e6c9bdb6ea42ee35d9d90f1c8496f92f7832
juniorcintra <junior.developer@outlook.com.br>
2019-03-13 09:41:23 -0300

    rota resolvida para mostrar os produtos na home

--- index.php
+++ index.php
@@ -5,11 +5,6 @@ session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
-use \Hcode\Model\User;
-use \Hcode\Model\Category;
-use \Hcode\Model\Product;
 
 $app = new Slim();
 
--- site.php
+++ site.php
@@ -6,12 +6,14 @@ use \Hcode\Model\Product;
 
 $app->get('/', function() {
 
-	$products = new Product::listAll();
+	$product = new Product();
 	
+	$products = Product::listAll();
+
 	$page = new Page();
 
 	$page->setTpl("index", [
-		'products'=>$products
+		"products"=>$products
 	]);	
 
 });
